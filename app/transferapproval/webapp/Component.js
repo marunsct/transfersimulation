c0edcc8a327caf25635dfe0afe201c81822522ba
TCS_ADMIN19 this-default-was-not-configured.invalid <TCS_ADMIN19>
2022-03-02 08:16:03 +0000

    Transfer Approve basic skeleton

--- app/transferapproval/webapp/Component.js
+++ app/transferapproval/webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
-	"transferapproval/model/models"
-], function (UIComponent, Device, models) {
+	"transferapproval/model/models",
+	"./controller/ErrorHandler"
+], function (UIComponent, Device, models, ErrorHandler) {
 	"use strict";
 
 	return UIComponent.extend("transferapproval.Component", {
@@ -25,6 +26,55 @@ sap.ui.define([
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
-		}
+
+			// set application model
+			var oApplicationModel = new sap.ui.model.json.JSONModel({});
+			this.setModel(oApplicationModel, "applicationModel");
+
+			// initialize the error handler with the component
+			this._oErrorHandler = new ErrorHandler(this);            
+
+            sap.ui.getCore().attachLocalizationChanged(function(oEvent){
+                var oChanges = oEvent.getParameter("changes");    
+                if (oChanges && oChanges.language){
+                    this._bundle = sap.ui.getCore().getLibraryResourceBundle("sap.ui.comp",oChanges.language);
+                  //  this.rerender();
+                }
+            }.bind(this));            
+
+		},
+		/**
+		 * The component is destroyed by UI5 automatically.
+		 * In this method, the ErrorHandler is destroyed.
+		 * @public
+		 * @override
+		 */
+		destroy : function () {
+			this._oErrorHandler.destroy();
+			// call the base component's destroy function
+			UIComponent.prototype.destroy.apply(this, arguments);
+		},
+
+		/**
+		 * This method can be called to determine whether the sapUiSizeCompact or sapUiSizeCozy
+		 * design mode class should be set, which influences the size appearance of some controls.
+		 * @public
+		 * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
+		 */
+		getContentDensityClass : function() {
+			if (this._sContentDensityClass === undefined) {
+				// check whether FLP has already set the content density class; do nothing in this case
+				// eslint-disable-next-line sap-no-proprietary-browser-api
+				if (document.body.classList.contains("sapUiSizeCozy") || document.body.classList.contains("sapUiSizeCompact")) {
+					this._sContentDensityClass = "";
+				} else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					// "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		}        
 	});
 });
---
+++ app/transferapproval/webapp/controller/App.controller.js
@@ -0,0 +1,60 @@
+sap.ui.define([
+    "./BaseController",
+    "sap/ui/model/json/JSONModel"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+    function (Controller, JSONModel) {
+        "use strict";
+
+        return Controller.extend("transferapproval.controller.App", {
+           
+            onInit: function () {
+
+
+                var oViewModel,
+                    oListSelector = this.getOwnerComponent().oListSelector,
+                    iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
+
+                oViewModel = new JSONModel({
+                    busy: true,
+                    delay: 0
+                });
+                this.getView().setModel(oViewModel, "appView")
+                // apply content density mode to root view
+                this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+
+                this.oRouter = this.getOwnerComponent().getRouter();
+                this.oRouter.attachRouteMatched(this.onRouteMatched, this);
+                return new Promise(function (fnResolve) {
+
+                    var oModel, aPromises = [];
+                    oModel = this.getOwnerComponent().getModel("oData");
+                    aPromises.push(oModel.metadataLoaded);
+                    return Promise.all(aPromises).then(function () {
+                        oViewModel.setProperty("/busy", false);
+                        oViewModel.setProperty("/delay", iOriginalBusyDelay);
+                        fnResolve();
+                    });
+                }.bind(this));
+
+
+            },
+
+            onLogout: function(){
+
+               window.location.href = "/my/logout";
+
+            },
+            onLanguageChange: function(){
+                if(sap.ui.getCore().getConfiguration().getLanguage() === 'ja'){
+                    sap.ui.getCore().getConfiguration().setLanguage('en');
+
+                }else{
+                    sap.ui.getCore().getConfiguration().setLanguage('ja') ;
+                   
+                }
+            }
+        });
+    });
